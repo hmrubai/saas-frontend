26a5df84bd097a1b79229830a5cedb5b51bcedd5
hmrubai <rubai.mobarak@gmail.com>
2023-09-27 01:50:42 +0600

    Content Details get by API

--- src/app/content-subject-details/content-subject-details.component.ts
+++ src/app/content-subject-details/content-subject-details.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { AuthenticationService } from '../_services/authentication.service';
+import { environment } from '../../environments/environment';
 import { Router, ActivatedRoute } from '@angular/router';
+import { CommonService } from '../_services/common.service';
+import { Timer } from '../_models/timer';
+import { ToastrService } from 'ngx-toastr';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-content-subject-details',
@@ -8,15 +17,71 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ContentSubjectDetailsComponent implements OnInit {
 
-    content_id;
+    @BlockUI() blockUI: NgBlockUI;
+    is_authenticated = false;
+    courseList: Array<any> = [];
+    is_loaded = false;
+    user_id: any = '';
+    modalRef?: BsModalRef;
+
+    assetURL = environment.imageURL;
+
+    image_thumbnail = "assets/images/images.png";
+
+    video_url: any;
+    pdfFilePath: any;
+    modal_title = 'Video';
+    modalConfig: any = { class: 'gray modal-xl', backdrop: 'static' };
+
+    outlineDetails: any = {};
+
+    quizDetails: any = {};
+
+    public user_role = null;
+    public currentUser: any = {};
+
+    content_subject_id;
     constructor(
+        
+        private _service: CommonService,
+        private authService: AuthenticationService,
+        private toastr: ToastrService,
         private route: ActivatedRoute,
+        private router: Router,
+        private modalService: BsModalService,
+        private _sanitizer: DomSanitizer
     ) {
-        this.content_id = this.route.snapshot.paramMap.get("content_subject_id");
-        console.log(this.content_id)
+        if (this.authService.isAuthenticated()) {
+            this.is_authenticated = true;
+            this.currentUser = this.authService.currentUserDetails.value;
+            this.user_role = this.currentUser.user_type;
+            this.user_id = this.currentUser.id;
+        }
+
+        this.content_subject_id = this.route.snapshot.paramMap.get("content_subject_id");
+        console.log(this.content_subject_id)
     }
 
     ngOnInit(): void {
+        this.getOutlineDetails();
+    }
+
+    getOutlineDetails() {
+        this.blockUI.start('Loading...');
+        this.outlineDetails = {};
+        this.user_id = this.user_id ? this.user_id : 0;
+        this._service.get('website/content-outline-details-by-id/' + this.content_subject_id).subscribe(res => {
+            this.outlineDetails = res.data;
+
+            // if(this.courseDetails.thumbnail){
+            //     this.image_thumbnail = this.assetURL + this.courseDetails.thumbnail;
+            // }
+
+            this.is_loaded = true;
+            this.blockUI.stop();
+        }, err => {
+            this.blockUI.stop();
+        });
     }
 
 }
